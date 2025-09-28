e2d92d6a71aa142ad8899a40435005ed4879906f
Vong Ly <vongly62@gmail.com>
2025-09-28 14:20:17 -0700

    refactored flows

--- orchestrate/flows/full_transformation.py
+++ orchestrate/flows/full_transformation.py
@@ -8,7 +8,7 @@ sys.path.insert(0, str(parent_dir))
 
 from subflows.build_dbt_output_folders import build_dbt_output_folders_flow
 from subflows.dbt_run import dbt_run_flow
-from subflows.create_psql_views_flow import create_psql_views_flow
+from subflows.create_psql_views import create_psql_views_flow
 
 
 @flow(name='Full Transformation')
