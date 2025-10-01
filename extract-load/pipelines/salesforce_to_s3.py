e8c73898b80ff769a8c22ad830a93a072ae70d88
Vong Ly <vongly62@gmail.com>
2025-10-01 13:18:17 -0700

    fixing extract-load bug

--- extract-load/pipelines/salesforce_to_s3.py
+++ extract-load/pipelines/salesforce_to_s3.py
@@ -54,7 +54,7 @@ def run_pipeline(test=False):
             object_name=obj,
             api_call_session=api_call_session,
             incremental_attribute='SystemModstamp',
-            write_disposition=
+            write_disposition='append',
         ).create_resource()
 
             for obj in objects
